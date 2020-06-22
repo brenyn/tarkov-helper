143ca4159097c4e68045aacd724d3a0a484d62a9
brenyn <brenynk@gmail.com>
2020-06-22 17:22:28 -0400

    added tabs to ammo page

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+/**********  NAVBAR SLIDE IN FUNCTION **********/
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
@@ -21,8 +22,23 @@ const navSlide = () => {
         //burger animation
         burger.classList.toggle('toggle-burger');
     });
+}
 
+/********** TAB FUNCTION **********/
+const makeTabs = () => {
+    const tabs = document.querySelectorAll('[data-tab-target]');
+    const tabContents = document.querySelectorAll('[data-tab-content]');
 
+    tabs.forEach(tab =>{
+        tab.addEventListener('click', () => {
+            tabContents.forEach(tabContent => {
+                tabContent.classList.remove('active'); // on click remove active class from all elements of type [data-tab-content] 
+            });
+            const target = document.querySelector(tab.dataset.tabTarget);
+            target.classList.add('active'); // on click add active class to specified [data-tab-target]
+        });
+    });
 }
 
 navSlide();
+makeTabs();
