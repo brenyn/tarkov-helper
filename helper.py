13fbdaef0a05a07c4c52a5eef4ffd1acb4afe6a6
Brenyn <brenynk@gmail.com>
2020-07-07 02:36:24 -0400

    edited routes

--- helper.py
+++ helper.py
@@ -2,11 +2,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/index.html")
 def home():
   return render_template('index.html')
 
-@app.route("/ammo.html")
+@app.route("/ammo")
 def ammo():
     return render_template('ammo.html')
 
