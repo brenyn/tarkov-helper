04191e75abd5979c6587565340226c02b29d98cb
Brenyn <brenynk@gmail.com>
2020-07-07 15:22:35 -0400

    refactored ammo data structure

--- helper.py
+++ helper.py
@@ -1,41 +1,45 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-ammoTypes= [
-  {'762x39':
-    (
-      {
-      'round': 'HP',
-      'damage': 87,
-      'penetration':15
-      },
-      {
-        'round': 'US',
-        'damage':56,
-        'penetration':29
-      }
-    )
+ammoTypes = [
+  {
+    'type':'762x39',
+    'rounds':
+      [
+        {
+          'round_name':'HP',
+          'damage':87,
+          'penetration':15
+        },
+        {
+          'round_name':'US',
+          'damage':56,
+          'penetration':29
+        }
+      ]
   },
-  {'762x51':
-    (
-      {
-      'round': 'M62',
-      'damage': 87,
-      'penetration':15
-      },
-      {
-        'round': 'M61',
-        'damage':56,
-        'penetration':29
-      }
-    )
+  {
+    'type':'762x51',
+    'rounds':
+      [
+        {
+          'round_name':'HP',
+          'damage':87,
+          'penetration':15
+        },
+        {
+          'round_name':'US',
+          'damage':56,
+          'penetration':29
+        }
+      ]
   },
 ]
 
-
 # for ammo in ammoTypes:
-# 	for round in ammo.items():
-# 		print(round[1][0]['round'])
+# 	print(ammo['type'])
+# 	for i in range(len(ammo['rounds'])):
+# 		print(ammo['rounds'][i]['round_name'])
 
 @app.route("/")
 def home():
