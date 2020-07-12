f0e7a134532a6b20efaa45fb5f91a8adf363d607
Brenyn <brenynk@gmail.com>
2020-07-11 20:56:56 -0400

    created database

--- helper.py
+++ helper.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config['SECRET_KEY'] = '2a9421a498f13bd18ce8cc0851dd7413'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
 db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False) #emails will be optional
+  password = db.Column(db.String(60), nullable=False)
+
+  def __repr__(self):
+    return f"User('{self.username}','{self.email}'"
+
 
 ammoTypes = [
   {
