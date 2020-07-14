08f82e2f145342c963b41fb06d360fe691d5ac38
Brenyn <brenynk@gmail.com>
2020-07-14 16:13:13 -0400

    updated form styling slightly, made email optional

--- helper/forms.py
+++ helper/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Optional
 from helper.models import User
 
 class RegistrationForm(FlaskForm):
   username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
 
-  email = StringField('Email', validators=[DataRequired(), Email()]) # make this optional
+  email = StringField('Email', validators=[Optional(), Email()]) # make this optional
 
   password = PasswordField('Password', validators=[DataRequired()])
 
