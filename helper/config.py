2839c7b23b69b0c42d29d4710ba72df8e6e09cb0
Brenyn <brenynk@gmail.com>
2020-07-15 20:29:05 -0400

    moved configuration to config file

--- helper/config.py
+++ helper/config.py
@@ -1,8 +1,10 @@
+import os
+
 class Config:
-  app.config['SECRET_KEY'] = '2a9421a498f13bd18ce8cc0851dd7413'
-  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-  app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-  app.config['MAIL_PORT'] = 465
-  app.config['MAIL_USE_SSL'] = True
-  app.config['MAIL_USERNAME'] = os.environ.get('HELPER_MAIL_USERNAME')
-  app.config['MAIL_PASSWORD'] = os.environ.get('HELPER_MAIL_PASSWORD')
+  SECRET_KEY = os.environ.get('HELPER_SECRET_KEY')
+  SQLALCHEMY_DATABASE_URI = os.environ.get('HELPER_DATABASE_URI')
+  MAIL_SERVER = 'smtp.gmail.com'
+  MAIL_PORT = 465
+  MAIL_USE_SSL = True
+  MAIL_USERNAME = os.environ.get('HELPER_MAIL_USERNAME')
+  MAIL_PASSWORD = os.environ.get('HELPER_MAIL_PASSWORD')
