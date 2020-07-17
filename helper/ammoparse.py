78099d056cd26c4988f97c6c9117b3e3025e531b
Brenyn <brenynk@gmail.com>
2020-07-17 16:46:36 -0400

    added list of calibers

--- helper/ammoparse.py
+++ helper/ammoparse.py
@@ -15,8 +15,25 @@ for ammo in ammotypes:
   db.session.commit()
 
 ammoTypes= []
-calibers=['']
-# for caliber in calibers
+calibers=['12 Gauge Shot',
+  '12 Gauge Slugs',
+  '20 Gauge',
+  '9x18mm',
+  '7.62x25mm',
+  '9x19mm',
+  '0.45',
+  '9x21mm',
+  '5.7x28 mm',
+  '4.6x30 mm',
+  '9x39mm',
+  '0.366',
+  '5.45x39 mm',
+  '5.56x45 mm',
+  '7.62x51 mm',
+  '7.62x54R',
+  '12.7x55 mm',]
+for caliber in calibers:
+  print(caliber)
 #   append dictionary to ammoTypes list w/ keys 'type': caliber, 'rounds':[]
 #   query by caliber
 #   append each round of caliber to list of dictionaries containing round name/stats
