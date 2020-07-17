d9e7e03fa0fef0189c087b74a7c2b955e9bdc816
Brenyn <brenynk@gmail.com>
2020-07-17 18:04:14 -0400

    update tables with data from db

--- helper/routes.py
+++ helper/routes.py
@@ -5,40 +5,6 @@ from helper.models import User, Ammo
 from flask_login import login_user, current_user, logout_user, login_required
 from flask_mail import Message
 
-ammoTypes = [
-  {
-    'type':'762x39',
-    'rounds':
-      [
-        {
-          'round_name':'HP',
-          'damage':87,
-          'penetration':15
-        },
-        {
-          'round_name':'US',
-          'damage':56,
-          'penetration':29
-        }
-      ]
-  },
-  {
-    'type':'762x51',
-    'rounds':
-      [
-        {
-          'round_name':'M61',
-          'damage':99,
-          'penetration':98
-        },
-        {
-          'round_name':'M62',
-          'damage':89,
-          'penetration':88
-        }
-      ]
-  },
-]
 
 ##### HOME ROUTE #####
 @app.route("/")
@@ -46,6 +12,41 @@ def home():
   return render_template('index.html')
 
 ##### AMMO ROUTE #####
+ammoTypes= []
+calibers=['12 Gauge Shot',
+  '12 Gauge Slugs',
+  '20 Gauge',
+  '9x18mm',
+  '7.62x25mm',
+  '9x19mm',
+  '0.45',
+  '9x21mm',
+  '5.7x28 mm',
+  '4.6x30 mm',
+  '9x39mm',
+  '0.366',
+  '5.45x39 mm',
+  '5.56x45 mm',
+  '7.62x51 mm',
+  '7.62x54R',
+  '12.7x55 mm',]
+
+class Caliber:
+  def __init__(self, caliber):
+    self.caliber = caliber
+    self.div_id = caliber.replace(' ', '-').replace('.','-').lower()
+    self.rounds=[]
+
+for caliber in calibers:
+  # append dictionary to ammoTypes list w/ keys 'type': caliber, 'rounds':[]
+  appendCaliber= Caliber(caliber)
+  # query by caliber
+  for ammo in Ammo.query.filter_by(ammo_type=caliber):
+    # print(ammo.round)
+    # append each round of caliber to list of dictionaries containing round name/stats
+    appendCaliber.rounds.append(dict(round=ammo.round, damage=ammo.damage,penetration=ammo.penetration,armor_damage=ammo.armor_damage,frag_chance=ammo.frag_chance))
+  ammoTypes.append(appendCaliber)
+  
 @app.route("/ammo")
 def ammo():
     return render_template('ammo.html', ammoTypes=ammoTypes, title='Ammo')
